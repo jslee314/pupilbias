5fd7fb201c88a6b42feab1c6baef35a9c347095b
jslee314 <jslee_314@naver.com>
2021-08-04 21:32:58 +0900

    MyApplication

---
+++ app/src/main/java/com/jslee/pupilbias/MyApplication.kt
@@ -0,0 +1,25 @@
+package com.jslee.pupilbias
+
+import android.app.Application
+import com.jslee.pupilbias.di.AppComponent
+import com.jslee.pupilbias.di.DaggerAppComponent
+
+
+class MyApplication: Application() {
+
+    // 프로젝트의 모든 활동에서 사용할 AppComponent 인스턴스
+    val appComponent: AppComponent by lazy {
+        initializeComponent()
+    }
+
+    fun initializeComponent(): AppComponent {
+        // create() 함수로 Component 구현
+        // Factory 생성자를 사용하여 AppComponent의 인스턴스를 만듦
+        // 그래프에서 Context로 사용될 applicationContext를 전달
+        return DaggerAppComponent.factory().create(applicationContext)
+
+    }
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
