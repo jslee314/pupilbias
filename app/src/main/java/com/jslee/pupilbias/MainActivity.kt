352b6e0623dd6c0fb99994769a99fd937f0c9d52
jslee314 <jslee_314@naver.com>
2021-08-04 15:23:43 +0900

    androidx로 수정

--- app/src/main/java/com/jslee/pupilbias/MainActivity.kt
+++ app/src/main/java/com/jslee/pupilbias/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.jslee.pupilbias
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.jslee.pupilbias.databinding.ActivityMainBinding
 
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val FINISH_INTERVAL_TIME = 2000L
+    private var backPressedTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+
+
+    override fun onBackPressed() {
+        var tempTime:Long = System.currentTimeMillis();
+        var intervalTime = tempTime - backPressedTime;
+
+        if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime) {
+            finish()
+        } else {
+            backPressedTime = tempTime
+            super.onBackPressed()
+        }
     }
 }
