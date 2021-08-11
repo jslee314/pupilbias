91acd9a79ae759f436fb7582d31e6f65e7dfed8f
jslee314 <jslee_314@naver.com>
2021-08-11 20:18:18 +0900

    s

--- app/src/main/java/tensorflowlite/model/SegmentationModelExecutor.kt
+++ app/src/main/java/tensorflowlite/model/SegmentationModelExecutor.kt
@@ -65,24 +65,24 @@ class SegmentationModelExecutor(context: Context, mode: Int, private var useGPU:
                 NUM_CLASSES = 3
                 imageSegmentationModel = "iris_pupil_v1_model.tflite"
             }
-            PUPIL_MODEL_128 -> {
-                IMAGE_WIDTH = 128
-                IMAGE_HEIGHT = 128
-                MASK_WIDTH = 128
-                MASK_HEIGHT = 128
-                IMAGE_MEAN = 0.0f
-                IMAGE_STD = 255.0f
-                imageSegmentationModel = "pupil_model_v1.tflite"
-            }
+//            PUPIL_MODEL_128 -> {
+//                IMAGE_WIDTH = 128
+//                IMAGE_HEIGHT = 128
+//                MASK_WIDTH = 128
+//                MASK_HEIGHT = 128
+//                IMAGE_MEAN = 0.0f
+//                IMAGE_STD = 255.0f
+//                imageSegmentationModel = "pupil_model_v1.tflite"
+//            }
         }
         if(mode == PUPIL_MODEL) {
             segmentColors[0] = Color.TRANSPARENT
             segmentColors[1] = Color.BLUE
         }
-        if(mode == PUPIL_MODEL_128) {
-            segmentColors[0] = Color.BLACK
-            segmentColors[1] = Color.WHITE
-        }
+//        if(mode == PUPIL_MODEL_128) {
+//            segmentColors[0] = Color.BLACK
+//            segmentColors[1] = Color.WHITE
+//        }
 
         interpreter = getInterpreter(context, imageSegmentationModel, useGPU)
         segmentationMasks = ByteBuffer.allocateDirect(1 * MASK_WIDTH * MASK_HEIGHT * NUM_CLASSES * 4)
@@ -135,25 +135,27 @@ class SegmentationModelExecutor(context: Context, mode: Int, private var useGPU:
                     formatExecutionLog(),
                     itemsFound
                 )
-            } else if (mode == PUPIL_MODEL_128) {
-                val (pupilIrisMask, pupilMask, itemsFound) =
-                    convertBytebufferMaskToBitmap(
-                        segmentationMasks, MASK_WIDTH, MASK_HEIGHT
-                    )
-                maskFlatteningTime = SystemClock.uptimeMillis() - maskFlatteningTime
-                Log.d(TAG, "Time to flatten the mask result $maskFlatteningTime")
-
-                fullTimeExecutionTime = SystemClock.uptimeMillis() - fullTimeExecutionTime
-                Log.d(TAG, "Total time execution $fullTimeExecutionTime")
-
-                return ModelExecutionResultVO(
-                    pupilIrisMask,
-                    scaledBitmap,
-                    pupilMask,
-                    formatExecutionLog(),
-                    itemsFound
-                )
-            } else {
+            }
+//            else if (mode == PUPIL_MODEL_128) {
+//                val (pupilIrisMask, pupilMask, itemsFound) =
+//                    convertBytebufferMaskToBitmap(
+//                        segmentationMasks, MASK_WIDTH, MASK_HEIGHT
+//                    )
+//                maskFlatteningTime = SystemClock.uptimeMillis() - maskFlatteningTime
+//                Log.d(TAG, "Time to flatten the mask result $maskFlatteningTime")
+//
+//                fullTimeExecutionTime = SystemClock.uptimeMillis() - fullTimeExecutionTime
+//                Log.d(TAG, "Total time execution $fullTimeExecutionTime")
+//
+//                return ModelExecutionResultVO(
+//                    pupilIrisMask,
+//                    scaledBitmap,
+//                    pupilMask,
+//                    formatExecutionLog(),
+//                    itemsFound
+//                )
+//            }
+            else {
                 val (maskImageApplied, maskOnly, itemsFound) =
                     convertBytebufferMaskToBitmap(
                         segmentationMasks, MASK_WIDTH, MASK_HEIGHT, scaledBitmap,
