f0035829f33160ad5d92c68071df3b375168da00
jslee314 <jslee_314@naver.com>
2021-08-15 23:57:16 +0900

    pupilParamColor

--- app/src/main/java/com/jslee/pupilbias/data/constant/AppDataConstants.kt
+++ app/src/main/java/com/jslee/pupilbias/data/constant/AppDataConstants.kt
@@ -8,6 +8,12 @@ class AppDataConstants {
         val pupilCenterColor = Scalar(255.0, 255.0, 0.0)
         val pupilCircleColor = Scalar(255.0, 0.0, 0.0)
         val pupilRectColor = Scalar(0.0, 0.0, 255.0)
+
+        val pupilParam1Color = Scalar(255.0, 0.0, 255.0)
+        val pupilParam2Color = Scalar(255.0, 255.0, 0.0)
+        val pupilParam3Color = Scalar(0.0, 255.0, 255.0)
+
+
     }
 
 
